2afa348970ec6fd53807c3b93c7d296d8cf78384
kharelranjan572-boop <kharelranjan572@gmail.com>
2026-02-10 22:54:11 +0545

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-
+app.use(express.json());
 // Your route
 app.get("/", (req, res) => {
   res.send(`
@@ -12,33 +12,6 @@ app.get("/", (req, res) => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Express App</title>
-        <style>
-          body {
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-            background: linear-gradient(135deg, #0f172a, #020617);
-            color: #e5e7eb;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-          }
-          .card {
-            background: #020617;
-            padding: 40px;
-            border-radius: 14px;
-            box-shadow: 0 20px 50px rgba(0, 0, 0, 0.6);
-            text-align: center;
-            max-width: 420px;
-          }
-          h1 {
-            margin-bottom: 10px;
-            font-size: 28px;
-          }
-          p {
-            opacity: 0.8;
-          }
-        </style>
       </head>
       <body>
         <div class="card">
